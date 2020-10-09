09e786bce6e5547cfc298b0061290986a71f5bf2
berberin <52871672+berberin@users.noreply.github.com>
2020-10-09 23:01:16 +0700

    Add search_result_screen

--- lib/ui/pages/search_result_screen.dart
+++ lib/ui/pages/search_result_screen.dart
@@ -1,18 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:shofar/providers/campaign_provider.dart';
+
+import 'home_srceen.dart';
 
 class SearchResultScreen extends StatefulWidget {
-  final List<String> campaigns;
+  final String keyword;
 
-  const SearchResultScreen({Key key, this.campaigns}) : super(key: key);
+  const SearchResultScreen({Key key, this.keyword}) : super(key: key);
   @override
   _SearchResultScreenState createState() => _SearchResultScreenState();
 }
 
 class _SearchResultScreenState extends State<SearchResultScreen> {
+  Future<List<String>> campaigns;
+
+  @override
+  void initState() {
+    super.initState();
+    CampaignProvider campaignProvider = CampaignProvider();
+    campaigns = campaignProvider.searchTitle(widget.keyword);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Search screen"),
+    return SafeArea(
+      child: Material(
+        color: Color(0xfffefefe),
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              Text(
+                "Search results: ${widget.keyword}",
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CampaignClusterOnBoard(
+                title: "Title: ${widget.keyword}",
+                future: campaigns,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
